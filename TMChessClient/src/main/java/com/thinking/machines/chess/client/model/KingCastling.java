fc4ec985b920b694d165fe312d968ac50b948e53
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-03 15:02:27 +0530

    Added KingCastling (stores castling related data)

---
+++ TMChessClient/src/main/java/com/thinking/machines/chess/client/model/KingCastling.java
@@ -0,0 +1,8 @@
+package com.thinking.machines.chess.client.model;
+public class KingCastling
+{
+public boolean checkCastling=true;
+public boolean kingMoved=false;
+public boolean leftRookMoved=false;
+public boolean rightRookMoved=false;
+}
