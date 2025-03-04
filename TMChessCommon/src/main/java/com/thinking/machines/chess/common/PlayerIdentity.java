5b534f9a59487263e632b210a5d694a5fffbb542
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-04 16:00:08 +0530

    Changed the properties name of PlayerIdentity.java

--- TMChessCommon/src/main/java/com/thinking/machines/chess/common/PlayerIdentity.java
+++ TMChessCommon/src/main/java/com/thinking/machines/chess/common/PlayerIdentity.java
@@ -1,6 +1,6 @@
 package com.thinking.machines.chess.common;
 public class PlayerIdentity implements java.io.Serializable
 {
-public String uuid;
-public String color;
+public String gameId;
+public String playerColor;
 }
