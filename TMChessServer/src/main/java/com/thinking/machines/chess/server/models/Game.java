e2bf439756dd25562bff0cc1ec510e40d68ef5de
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-05 14:19:26 +0530

    Moved Game.java again to server-side from common since it's server specific

---
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/models/Game.java
@@ -0,0 +1,12 @@
+package com.thinking.machines.chess.server.models;
+import com.thinking.machines.chess.common.Move;
+import java.util.*;
+public class Game implements java.io.Serializable
+{
+public String id;
+public String player1;
+public String player2;
+public byte[][] board;
+public byte activePlayer;
+public List<Move> moves;
+}
