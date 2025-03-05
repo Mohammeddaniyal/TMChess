8d0b3cb4b553e965bda43d39b9a4dcc5ad7bd32c
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-05 13:44:45 +0530

    Changed GameInit property String playerColor to byte PlayerColor

--- TMChessCommon/src/main/java/com/thinking/machines/chess/common/GameInit.java
+++ TMChessCommon/src/main/java/com/thinking/machines/chess/common/GameInit.java
@@ -2,6 +2,6 @@ package com.thinking.machines.chess.common;
 public class GameInit implements java.io.Serializable
 {
 public String gameId;
-public String playerColor;
+public byte playerColor;
 public byte[][] board ;
 }
