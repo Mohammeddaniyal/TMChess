59e91879daef8373d25b4c71988949f1b2acbd61
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-05 20:32:48 +0530

    PawnMoveValidator Made changes accordingly byte[][] board and removed JButton[][] tiles part since byte[][] represent board

--- TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/PawnMoveValidator.java
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/PawnMoveValidator.java
@@ -2,11 +2,11 @@ package com.thinking.machines.chess.server.validators;
 import javax.swing.*;
 public class PawnMoveValidator
 {
-public static boolean validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,JButton[][] tiles)
+public static boolean validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,byte[][] board)
 {
-String sourceIconName=tiles[startRowIndex][startColumnIndex].getActionCommand();
+byte sourcePiece=board[startRowIndex][startColumnIndex];
 boolean capture=false;
-if(sourceIconName.equals("blackPawn"))
+if(sourcePiece==-1)//blackPawn
 {
 if(destinationRowIndex<=startRowIndex)
 {
@@ -19,10 +19,10 @@ if(startRowIndex==1)//at begining allowing 2 steps
 if(destinationRowIndex-startRowIndex==2)//2 square tile forward validation
 {
 //not allowing to jump from a piece if it is in path of the pawn
-JButton piece=tiles[startRowIndex+1][startColumnIndex];
-String pieceName=piece.getActionCommand();
-if(pieceName.equals("")==false) return false;
-if(tiles[startRowIndex+2][startColumnIndex].getActionCommand().equals("")==false)return false;
+
+byte tile=board[startRowIndex+1][startColumnIndex];
+if(tile!=0) return false;
+if(board[startRowIndex+2][startColumnIndex]!=0)return false;
 }
 if((destinationRowIndex-startRowIndex!=2 && destinationRowIndex-startRowIndex!=1))
 {
@@ -35,7 +35,7 @@ return false;
 // restrictring more than one tile movement of the pawn
 return false;
 }
-if(startRowIndex==destinationRowIndex-1 && startColumnIndex==destinationColumnIndex && tiles[startRowIndex+1][startColumnIndex].getActionCommand().equals("")==false)
+if(startRowIndex==destinationRowIndex-1 && startColumnIndex==destinationColumnIndex && board[startRowIndex+1][startColumnIndex]!=0)
 {
 //restricting movement if any piece is in the forward path
 return false;
@@ -43,14 +43,11 @@ return false;
 //capture  part of pawn(en passant)diagonal right or left
 if( (startRowIndex+1==destinationRowIndex) && ((startColumnIndex-1==destinationColumnIndex) || (startColumnIndex+1==destinationColumnIndex)) )
 {
-
-String sourcePieceColor=tiles[startRowIndex][startColumnIndex].getActionCommand().substring(0,5);
-String targetIconName=tiles[destinationRowIndex][destinationColumnIndex].getActionCommand();
-String targetPieceColor="";
-if(targetIconName.equals("")) return false;
-targetPieceColor=targetIconName.substring(0,5);
-if(sourcePieceColor.equals(targetPieceColor))
+byte targetPiece=board[destinationRowIndex][destinationColumnIndex];
+if(targetPiece==0) return false;// no piece to capture by which en passant can be performed
+if( (sourcePiece>0 && targetPiece>0) || (sourcePiece<0 && targetPiece<0))
 {
+//not allowing the capture of same piece color
 return false;
 }
 capture=true;
@@ -61,7 +58,7 @@ if(capture!=true && startColumnIndex!=destinationColumnIndex)
 return false;
 }
 //black pawn part ends here
-}else if(sourceIconName.equals("whitePawn"))
+}else if(sourcePiece==1)
 {
 if(startRowIndex<=destinationRowIndex)
 {
@@ -73,11 +70,10 @@ if(startRowIndex==6)//at begining allowing 2 steps
 if(startRowIndex-destinationRowIndex==2)//2 square tile forward validation
 {
 //not allowing to jump from a piece if it is in path of the pawn
-JButton piece=tiles[startRowIndex-1][startColumnIndex];
-String pieceName=piece.getActionCommand();
+byte tile=board[startRowIndex-1][startColumnIndex];
 //checking the path isn't blocked
-if(pieceName.equals("")==false) return false;
-if(tiles[startRowIndex-2][startColumnIndex].getActionCommand().equals("")==false)return false;
+if(tile!=0) return false;
+if(board[startRowIndex-2][startColumnIndex]!=0)return false;
 }
 if(startRowIndex-destinationRowIndex!=2 && startRowIndex-destinationRowIndex!=1)
 {
@@ -92,20 +88,18 @@ if(startRowIndex!=6 && startRowIndex-destinationRowIndex!=1)
 return false;
 }
 //path blocking in forward move case of pawn
-if(startRowIndex==destinationRowIndex+1 && startColumnIndex==destinationColumnIndex &&  tiles[startRowIndex-1][startColumnIndex].getActionCommand().equals("")==false)
+if(startRowIndex==destinationRowIndex+1 && startColumnIndex==destinationColumnIndex &&  board[startRowIndex-1][startColumnIndex]!=0)
 {
 return false;
 }
 //capture  enpassant
 if( (startRowIndex-1==destinationRowIndex) && ((startColumnIndex-1==destinationColumnIndex) || (startColumnIndex+1==destinationColumnIndex)) )
 {
-String sourcePieceColor=tiles[startRowIndex][startColumnIndex].getActionCommand().substring(0,5);
-String targetIconName=tiles[destinationRowIndex][destinationColumnIndex].getActionCommand();
-String targetPieceColor="";
-if(targetIconName.equals("")) return false;
-targetPieceColor=targetIconName.substring(0,5);
-if(sourcePieceColor.equals(targetPieceColor))
+byte targetPiece=board[destinationRowIndex][destinationColumnIndex];
+if(targetPiece==0) return false;
+if( (sourcePiece>0 && targetPiece>0) || (sourcePiece<0 && targetPiece<0))
 {
+//not allowing the capture of same piece color
 return false;
 }
 capture=true;
