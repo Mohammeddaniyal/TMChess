704397c2a260b86943d911f6f6b92fe31f1abfbc
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-05 14:20:12 +0530

    Moved KingCastling to server-side for better validation and security

---
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/models/KingCastling.java
@@ -0,0 +1,8 @@
+package com.thinking.machines.chess.server.models;
+public class KingCastling
+{
+public boolean checkCastling=true;
+public boolean kingMoved=false;
+public boolean leftRookMoved=false;
+public boolean rightRookMoved=false;
+}
