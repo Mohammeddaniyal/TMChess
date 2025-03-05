fadd0ff62f66b010032b4de9c3248a96438ad77f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-05 12:27:13 +0530

    Moved KingCastling from client to server-side for centralized rule enforcement

--- TMChessClient/src/main/java/com/thinking/machines/chess/client/model/KingCastling.java
+++
@@ -1,8 +0,0 @@
-package com.thinking.machines.chess.client.model;
-public class KingCastling
-{
-public boolean checkCastling=true;
-public boolean kingMoved=false;
-public boolean leftRookMoved=false;
-public boolean rightRookMoved=false;
-}
