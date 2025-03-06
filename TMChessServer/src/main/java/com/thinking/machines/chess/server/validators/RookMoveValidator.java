c7bb02dca9735893d1d2b249704670e9d0f33d0f
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-06 20:22:07 +0530

    Refactored RookMoveValidator.java handled accordingly byte[][] board instead of JButton[][] and String comparision made comparision according to byte

--- TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/RookMoveValidator.java
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/RookMoveValidator.java
@@ -2,9 +2,9 @@ package com.thinking.machines.chess.server.validators;
 import javax.swing.*;
 public class RookMoveValidator
 {
-public static boolean validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,byte[][] board)
+public static byte validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,byte[][] board)
 {
-if(startRowIndex!=destinationRowIndex && startColumnIndex!=destinationColumnIndex) return false;//restricting rook diagonal movement
+if(startRowIndex!=destinationRowIndex && startColumnIndex!=destinationColumnIndex) return 0;//restricting rook diagonal movement
 byte tile;
 if(startColumnIndex==destinationColumnIndex)//vertical movement
 {
@@ -13,14 +13,14 @@ if(startRowIndex<destinationRowIndex)
 for(int e=startRowIndex+1;e<destinationRowIndex;e++)
 {
 tile=board[e][startColumnIndex];
-if(tile!=0) return false;
+if(tile!=0) return 0;
 }
 }else
 {
 for(int e=startRowIndex-1;e>destinationRowIndex;e--)
 {
 tile=board[e][startColumnIndex];
-if(tile!=0) return false;
+if(tile!=0) return 0;
 }
 }
 }else if(startRowIndex==destinationRowIndex)//horizontal movement
@@ -30,17 +30,17 @@ if(startColumnIndex<destinationColumnIndex)
 for(int f=startColumnIndex+1;f<destinationColumnIndex;f++)
 {
 tile=board[startRowIndex][f];
-if(tile!=0) return false;
+if(tile!=0) return 0;
 }
 }else
 {
 for(int f=startColumnIndex-1;f>destinationColumnIndex;f--)
 {
 tile=board[startRowIndex][f];
-if(tile!=0) return false;
+if(tile!=0) return 0;
 }
 }
 }
-return true;
+return 1;
 }
 }
