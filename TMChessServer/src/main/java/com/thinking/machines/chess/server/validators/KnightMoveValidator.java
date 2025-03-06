48bfb33c75b926cb0bbcb5277f3b47285a329944
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-06 20:21:26 +0530

    Refactored KnightMoveValidator.java handled accordingly byte[][] board instead of JButton[][] and String comparision made comparision according to byte

--- TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/KnightMoveValidator.java
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/KnightMoveValidator.java
@@ -1,20 +1,20 @@
 package com.thinking.machines.chess.server.validators;
 public class KnightMoveValidator
 {
-public static boolean validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex)
+public static byte validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex)
 {
 //mandatoring knight to move in L-shape movement
 int d1=(startRowIndex<destinationRowIndex)?destinationRowIndex-startRowIndex:startRowIndex-destinationRowIndex;
 int d2=(startColumnIndex<destinationColumnIndex)?destinationColumnIndex-startColumnIndex:startColumnIndex-destinationColumnIndex;
-if(d1!=1 && d1!=2) return false;
+if(d1!=1 && d1!=2) return 0;
 if(d1==1)//two square right or left and one square bottom or top
 {
-if(d2!=2) return false;
+if(d2!=2) return 0;
 }else //two square top or bottom and one square left or right
 if(d1==2)
 {
-if(d2!=1) return false;
+if(d2!=1) return 0;
 }
-return true;
+return 1;
 }
 }
