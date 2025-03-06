0cfdc0dcdc1a4c5e1698760a34491e59992a554c
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-06 20:22:56 +0530

    Refactoring KingMoveValidator.java started working on handling according to byte

--- TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/KingMoveValidator.java
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/KingMoveValidator.java
@@ -6,48 +6,50 @@ import java.util.*;
 public class KingMoveValidator
 {
 private KingMoveValidator(){};
-public static boolean validateMove(JButton [][]tiles,int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,KingCastling kingCastling)
+public static byte validateMove(byte[][] board,int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,KingCastling kingCastling)
 {
 //castling part
 if(kingCastling.checkCastling==true)
 {
-String kingName=tiles[startRowIndex][startColumnIndex].getActionCommand();
-if(kingName.equals("blackKing"))
+byte kingPiece=board[startRowIndex][startColumnIndex];
+if(kingPiece==-6)//black king
 {
-
 //king's side castling move arrived
 if(startRowIndex==0 && startColumnIndex==4 && destinationRowIndex==0 && destinationColumnIndex==6)
 {
 System.out.println("black (right)rook moved : "+kingCastling.rightRookMoved);
 if(kingCastling.kingMoved==true || kingCastling.rightRookMoved==true)
 {
-return false;
+return 0;
 }
 //checking if tiles are empty
-if(tiles[0][5].getActionCommand().equals("")==false || tiles[0][6].getActionCommand().equals("")==false )
+if(board[0][5]!=0 || tiles[0][6]!=0 )
 {
-return false;
+return 0;
 }
 //checking is king is in checkmate or not
-ArrayList<PieceMoves> piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"black",startRowIndex,startColumnIndex,false);
+//one of the rule of castling the king cannot be in check
+ArrayList<PieceMoves> piecesMoves=CheckmateDetector.isPieceInDanger(board,"black",startRowIndex,startColumnIndex,false);
 if(piecesMoves.size()!=0) 
 {
-return false;
+return 0;
 }
-//not to check is tile f8 and g8 are not in any threat
+// Another rule of castling that the destination tile of king should not be in check
+//now to check is tile f8 and g8 are not in any threat
 
+//for tile f8
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"black",0,5,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
 //for tile g8
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"black",0,6,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
-return true;
+return 1;
 }// king's side castling ends here
 
 //queen's side castling move arrived
@@ -55,33 +57,33 @@ if(startRowIndex==0 && startColumnIndex==4 && destinationRowIndex==0 && destinat
 {
 if(kingCastling.kingMoved==true || kingCastling.leftRookMoved==true)
 {
-return false;
+return 0;
 }
 //checking if tiles are empty
 if(tiles[0][1].getActionCommand().equals("")==false || tiles[0][2].getActionCommand().equals("")==false || tiles[0][3].getActionCommand().equals("")==false )
 {
-return false;
+return 0;
 }
 //checking is king is in checkmate or not
 ArrayList<PieceMoves> piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"black",startRowIndex,startColumnIndex,false);
 if(piecesMoves.size()!=0) 
 {
-return false;
+return 0;
 }
 //now to check is tile c1 and d1 are not in any threat
 
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"black",0,2,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
 //for tile g8
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"black",0,3,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
-return true;
+return 1;
 }// queen's side castling ends here
 }//castling of black king part ends here
 
@@ -95,33 +97,33 @@ if(startRowIndex==7 && startColumnIndex==4 && destinationRowIndex==7 && destinat
 System.out.println("white (right)rook moved : "+kingCastling.rightRookMoved);
 if(kingCastling.kingMoved==true || kingCastling.rightRookMoved==true)
 {
-return false;
+return 0;
 }
 //checking if tiles are empty
 if(tiles[7][5].getActionCommand().equals("")==false || tiles[7][6].getActionCommand().equals("")==false )
 {
-return false;
+return 0;
 }
 //checking is king is in checkmate or not
 ArrayList<PieceMoves> piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"white",startRowIndex,startColumnIndex,false);
 if(piecesMoves.size()!=0) 
 {
-return false;
+return 0;
 }
 //not to check is tile f8 and g8 are not in any threat
 
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"white",7,5,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
 //for tile g8
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"white",7,6,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
-return true;
+return 1;
 }// king's side castling ends here
 
 //queen's side castling move arrived
@@ -129,33 +131,33 @@ if(startRowIndex==7 && startColumnIndex==4 && destinationRowIndex==7 && destinat
 {
 if(kingCastling.kingMoved==true || kingCastling.leftRookMoved==true)
 {
-return false;
+return 0;
 }
 //checking if tiles are empty
 if(tiles[7][1].getActionCommand().equals("")==false || tiles[7][2].getActionCommand().equals("")==false || tiles[7][3].getActionCommand().equals("")==false )
 {
-return false;
+return 0;
 }
 //checking is king is in checkmate or not
 ArrayList<PieceMoves> piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"white",startRowIndex,startColumnIndex,false);
 if(piecesMoves.size()!=0) 
 {
-return false;
+return 0;
 }
 //now to check is tile c1 and d1 are not in any threat
 
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"white",7,2,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
 //for tile g8
 piecesMoves=CheckmateDetector.isPieceInDanger(tiles,"white",7,3,false);
 if(piecesMoves.size()!=0)
 {
-return false;
+return 0;
 }
-return true;
+return 1;
 }// queen's side castling ends here
 
 
@@ -167,11 +169,11 @@ int d=0;
 if(startColumnIndex==destinationColumnIndex)//moving one step veritcally
 {
 d=(startRowIndex<destinationRowIndex)?destinationRowIndex-startRowIndex:startRowIndex-destinationRowIndex;
-if(d!=1) return false;
+if(d!=1) return 0;
 }else if(startRowIndex==destinationRowIndex)//moving one step horizontally
 {
 d=(startColumnIndex<destinationColumnIndex)?destinationColumnIndex-startColumnIndex:startColumnIndex-destinationColumnIndex;
-if(d!=1) return false;
+if(d!=1) return 0;
 }
 else//moving one step diagonally
 {
@@ -179,9 +181,9 @@ int d1=(startRowIndex<destinationRowIndex)?destinationRowIndex-startRowIndex:sta
 int d2=(startColumnIndex<destinationColumnIndex)?destinationColumnIndex-startColumnIndex:startColumnIndex-destinationColumnIndex;
 if(d1!=1 || d2!=1) 
 {
-return false;
+return 0;
 }
 }
-return true;
+return 1;
 }
 }//class ends here (KingValidator)
