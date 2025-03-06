6279462990b0d6b50977a81b399d2575f83e03de
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-06 20:20:45 +0530

    Refactored BishopMoveValidator.java handled accordingly byte[][] board instead of JButton[][] and String comparision made comparision according to byte

--- TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/BishopMoveValidator.java
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/validators/BishopMoveValidator.java
@@ -2,10 +2,10 @@ package com.thinking.machines.chess.server.validators;
 import javax.swing.*;
 public class BishopMoveValidator
 {
-public static boolean validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,byte [][]board)
+public static byte validateMove(int startRowIndex,int startColumnIndex,int destinationRowIndex,int destinationColumnIndex,byte [][]board)
 {
 //restricting vertical and horizontal movement of the (bishop)
-if((startRowIndex==destinationRowIndex && startColumnIndex!=destinationColumnIndex) || (startRowIndex!=destinationRowIndex && startColumnIndex==destinationColumnIndex)) return false; 
+if((startRowIndex==destinationRowIndex && startColumnIndex!=destinationColumnIndex) || (startRowIndex!=destinationRowIndex && startColumnIndex==destinationColumnIndex)) return 0; 
 //validating movement of bishop diagonally on the same tile
 int d1=startRowIndex-destinationRowIndex;
 int d2=startColumnIndex-destinationColumnIndex;
@@ -13,7 +13,7 @@ if(d1<0) d1=d1*(-1);
 if(d2<0) d2=d2*(-1);
 if(d1!=d2)
 {
-return false;
+return 0;
 }
 //validating path blocker
 byte tile;
@@ -25,7 +25,7 @@ for(e=startRowIndex-1,f=startColumnIndex-1;e>destinationRowIndex;e--,f--)
 tile=board[e][f];
 if(tile!=0) 
 {
-return false;
+return 0;
 }
 }
 //path blocker for top-left ends here
@@ -37,7 +37,7 @@ for(e=startRowIndex-1,f=startColumnIndex+1;e>destinationRowIndex;e--,f++)
 tile=board[e][f];
 if(tile!=0)
 {
-return false;
+return 0;
 }
 }
 //path blocker for top-right ends here
@@ -49,7 +49,7 @@ for(e=startRowIndex+1,f=startColumnIndex-1;e<destinationRowIndex;e++,f--)
 tile=board[e][f];
 if(tile!=0) 
 {
-return false;
+return 0;
 }
 }
 //path blocker for bottom-left ends here
@@ -61,11 +61,11 @@ for(e=startRowIndex+1,f=startColumnIndex+1;e<destinationRowIndex;e++,f++)
 tile=board[e][f];
 if(tile!=0)
 {
-return false;
+return 0;
 }
 }
 //path blocker for bottom-right ends here
 }
-return true;
+return 1;
 }
 }
