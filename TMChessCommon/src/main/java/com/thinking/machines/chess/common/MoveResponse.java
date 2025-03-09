423b51e175e9d575ea38e4824a6761ad278859d9
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-09 15:03:49 +0530

    feat(MoveResponse) : increased a property to track pawnPromotion

--- TMChessCommon/src/main/java/com/thinking/machines/chess/common/MoveResponse.java
+++ TMChessCommon/src/main/java/com/thinking/machines/chess/common/MoveResponse.java
@@ -1,4 +1,4 @@
-package com.thinking.machines.chess.common;
+ package com.thinking.machines.chess.common;
 public class MoveResponse implements java.io.Serializable
 {
 public byte isValid;
@@ -8,4 +8,10 @@ public byte castlingType=0;
 //2 white queenside castling
 //3 black king side
 //4 black queen side
+public byte pawnPromotionTo=0;
+//0 represents no promotion
+//1 promote to Queen
+//2 promote to Rook
+//3 promote to Bishop
+//4 promote to Knight
 }
