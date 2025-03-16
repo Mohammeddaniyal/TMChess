13d38713e5f29f8409ca5213cb2838b2af0f1924
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-16 15:32:30 +0530

    Working(ChessUI) : working on adding of new column status

--- TMChessClient/src/main/java/com/thinking/machines/chess/client/ChessUI.java
+++ TMChessClient/src/main/java/com/thinking/machines/chess/client/ChessUI.java
@@ -413,8 +413,7 @@ return false;
 }
 public Class getColumnClass(int column)
 {
-if(column==0) return String.class;
-if(column==1) 
+if(column==0 || column==1) return String.class;
 return JButton.class;
 }
 public void setMembers(java.util.List<String> members)
