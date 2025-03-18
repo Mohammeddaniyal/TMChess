6bb0818cc4c37c3690009ee2bab5fee16342c5f3
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-18 20:51:35 +0530

    feat(StalemateDetector) : added done perfectly handled

--- TMChessServer/src/main/java/com/thinking/machines/chess/server/logic/StalemateDetector.java
+++ TMChessServer/src/main/java/com/thinking/machines/chess/server/logic/StalemateDetector.java
@@ -19,12 +19,33 @@ if((piece>0 && player==0) || (piece<0 && player==1)) continue;
 index=new byte[2];
 index[0]=e;
 index[1]=f;
+playerPiecesIndexes.add(index);
 }
 }
 byte isStalemate=0;
-
+KingCastling kingCastling;
+if(player==1)//white king
+{
+kingCastling=game.whiteKingCastling;
+}else
+{
+kingCastling=game.blackKingCastling;
+}
+//since it's stalemate we dont need to check for castling scenario
+kingCastling.checkCastling=false;
 for(byte []_index:playerPiecesIndexes)
 {
+byte fromX=_index[0];
+byte fromY=_index[1];
+//passing 1 as called from stalemate detection
+byte [][]possibleMoves=CheckmateDetector.getPossibleMoves(game.board,fromX,fromY,kingCastling,(byte)1);
+if(possibleMoves==null) isStalemate=1;
+else 
+{
+//since there's even a single legal move avilable so it means no stalemate
+isStalemate=0;
+break;
+}
 }
 
 return isStalemate;
