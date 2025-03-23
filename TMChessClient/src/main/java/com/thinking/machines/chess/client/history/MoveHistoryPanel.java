1b76f9f26de0142cc9fc9fe39d96d16a8e353df1
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-23 21:59:35 +0530

    Feat add this panel for displaying chess move history

---
+++ TMChessClient/src/main/java/com/thinking/machines/chess/client/history/MoveHistoryPanel.java
@@ -0,0 +1,29 @@
+package com.thinking.machines.chess.client.history;
+import javax.swing.*;
+import javax.swing.table.*;
+import java.awt.*;
+import java.awt.event.*;
+public class MoveHistoryPanel extends JPanel
+{
+private MoveHistoryTableModel moveHistoryTableModel;
+private JTable moveHistoryTable;
+private JScrollPane moveHistoryTableScrollPane;
+MoveHistoryPanel(byte firstPlayerColor)
+{
+moveHistoryTableModel=new MoveHistoryTableModel(firstPlayerColor);
+moveHistoryTable=new JTable(moveHistoryTableModel);
+moveHistoryTableScrollPane=new JScrollPane(moveHistoryTable,JScrollPane.VERTICAL_SCROLLBAR_ALWAYS,JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+setLayout(new BorderLayout());
+add(moveHistoryTableScrollPane);
+setVisible(true);
+}
+public void addBlackMove(String move)
+{
+moveHistoryTableModel.addBlackMove(move);
+}
+public void addWhiteMove(String move)
+{
+moveHistoryTableModel.addWhiteMove(move);
+}
+
+}
