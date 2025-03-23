98f51af3e4670b81da49c1fc7e174fd2750a58b8
Mohammeddaniyal <mohammeddaniyal453@gmail.com>
2025-03-23 21:16:30 +0530

    Feat(PGNConvertor) : working added upto king caslting , ambiguity

---
+++ TMChessClient/src/main/java/com/thinking/machines/chess/client/history/PGNConvertor.java
@@ -0,0 +1,42 @@
+package com.thinking.machines.chess.client.history;
+public class PGNConvertor
+{
+public static String convertMoveToPGN(Move move,byte isCapture)
+{
+StringBuilder pgn=new StringBuilder();
+
+if(move.castlingType==1 || move.castlingType==3) return "O-O";
+else if(move.castlingType==1 || move.castlingType==3) return "O-O-O";
+
+//determine piece notation
+char pieceChar=getPieceChar(move.piece);
+
+if(pieceChar!=' ') pgn.append(pieceChar);
+
+//handling ambiguity case
+//file ambiguity
+if(move.ambiguityType==1)
+{
+pgn.append(new String(move.fromX+1));
+}else if(move.ambiguityType==2){
+//rank ambiguity
+pgn.append(('a'+move.fromY));
+}
+
+//capture notation
+
+}
+
+private char getPieceChar(byte piece)
+{
+byte piece=(byte)((piece<0)?piece*-1:piece);
+return switch(piece){
+case 1 -> ' ',//pawn no letter
+case 2 -> 'N',
+case 3 -> 'B',
+case 4 -> 'R',
+case 5 -> 'Q',
+case 6 -> 'K'
+};
+}
+}
